9d8256c1c7687248923ad5459e63586a189df8d2
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2026-03-08 12:19:29 +0100

    Edit panel site layout

---
+++ site/collections/system-pages.php
@@ -0,0 +1,7 @@
+<?php
+
+return function ($kirby) {
+    $children = $kirby->site()->children();
+    return $children->filterBy('slug', 'home')
+        ->add($children->filterBy('slug', 'error'));
+};
